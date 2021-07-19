c7459497a5d5a32d429b0a75d7c5e19543738c6d
fernandopaz1 <ferpaz9292@gmail.com>
2021-07-18 23:39:54 -0300

    add: ejemplo de jsx components dentro de otros

--- src/index.js
+++ src/index.js
@@ -1,12 +1,29 @@
 import React from "react";
 import { render } from "react-dom";
 
+const Saludo = () => {
+    let aQuien = "mundo";
+    return <p>Hola {aQuien}</p>;
+};
+
 // funcion de javascript que retorna un componente de jsx vdom
+// los componenetes se invocan como si fueran tags de html
+// se puede usar self closing tags <Saludo /> si no es necesaro pasarle parametros
+
+// los componenetes que crea el usuario empiezan con mayusculas
+// mientras que losnativos empiezan con minuscula
+// guarda al llamar un componenete no es como un string
+// puede parecer como un salto de linea pero porque no estan en el mismo dom
 const App = () => {
-    return <h1>Hello, world!</h1>;
+    return (
+        <h1>
+            <Saludo />!
+        </h1>
+    );
 };
 
 // aca renderizamos el vdom que devuelve app en el elemento de
 // de html con id root definido en public/index.html
 // la etiqueta en la que se agrega es App
 render(<App />, document.getElementById("root"));
+
